748c0c30de4b4740600a5a02549ce5b47892fd6c
kevinhuanca <kevinhuanca75@gmail.com>
2023-06-10 14:59:03 -0300

    Update Producto.java

--- src/Entidades/Producto.java
+++ src/Entidades/Producto.java
@@ -2,27 +2,27 @@ package Entidades;
 
 public class Producto {
     private int id_producto;
-    private String nombre;
     private String descripcion;
     private double precio;
     private int stock;
+    private boolean estado;
 
     public Producto() {
     }
 
-    public Producto(String nombre, String descripcion, double precio, int stock) {
-        this.nombre = nombre;
+    public Producto(String descripcion, double precio, int stock, boolean estado) {
         this.descripcion = descripcion;
         this.precio = precio;
         this.stock = stock;
+        this.estado = estado;
     }
 
-    public Producto(int id_producto, String nombre, String descripcion, double precio, int stock) {
+    public Producto(int id_producto, String descripcion, double precio, int stock, boolean estado) {
         this.id_producto = id_producto;
-        this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.stock = stock;
+        this.estado = estado;
     }
 
     public int getId_producto() {
@@ -31,14 +31,6 @@ public class Producto {
 
     public void setId_producto(int id_producto) {
         this.id_producto = id_producto;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public String getDescripcion() {
@@ -65,9 +57,17 @@ public class Producto {
         this.stock = stock;
     }
 
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
     @Override
     public String toString() {
-        return "Producto{" + "id_producto=" + id_producto + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio + ", stock=" + stock + '}';
+        return "Producto{" + "id_producto=" + id_producto + ", descripcion=" + descripcion + ", precio=" + precio + ", stock=" + stock + ", estado=" + estado + '}';
     }
     
 }
