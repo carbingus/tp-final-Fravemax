cfbc1bead29583f155f5840e8a26913f5dfddba1
carbingus <exthalia2015@gmail.com>
2023-06-10 22:12:38 -0300

    productoooooo

--- src/Entidades/Venta.java
+++ src/Entidades/Venta.java
@@ -6,20 +6,32 @@ public class Venta {
     private int id_venta;
     private LocalDate fecha;
     private Cliente cliente;
+    private Producto producto;
 
     public Venta() {
     }
 
-    public Venta(LocalDate fecha, Cliente cliente) {
+    public Venta(LocalDate fecha, Cliente cliente, Producto producto) {
         this.fecha = fecha;
         this.cliente = cliente;
+        this.producto = producto;
     }
 
-    public Venta(int id_venta, LocalDate fecha, Cliente cliente) {
+    public Venta(int id_venta, LocalDate fecha, Cliente cliente, Producto producto) {
         this.id_venta = id_venta;
         this.fecha = fecha;
         this.cliente = cliente;
+        this.producto = producto;
     }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+    
 
     public int getId_venta() {
         return id_venta;
