dab870074fa50e2a1f59e8c0f972975bcbc4fc0b
kevinhuanca <kevinhuanca75@gmail.com>
2023-06-14 11:40:36 -0300

    Update DetalleCompraData.java

--- src/AccesoADatos/DetalleCompraData.java
+++ src/AccesoADatos/DetalleCompraData.java
@@ -1,5 +1,37 @@
 package AccesoADatos;
 
+import Entidades.DetalleCompra;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 public class DetalleCompraData {
+    private Connection conexion = null;
+
+    public DetalleCompraData() {
+        conexion = Conexion.getConexion();
+    }
     
+    public void guardarDetalleCompra(DetalleCompra dc) {
+        String sql = "INSERT INTO detallecompra(cantidad,precioCosto,idCompra,idProducto) VALUES (?,?,?,?);";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, dc.getCantidad());
+            ps.setDouble(2, dc.getPrecioCosto());
+            ps.setInt(3, dc.getCompra().getIdCompra());
+            ps.setInt(4, dc.getProducto().getIdProducto());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()){
+                dc.setIdDetalle(rs.getInt(1));
+            }
+            System.out.println("DetalleCompra guardado!");
+            ps.close();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: "+e.getLocalizedMessage());
+        }
+    }
 }
