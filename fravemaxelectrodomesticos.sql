c05de5f6565405851a084c26f9eb1d9b159affb2
kevinhuanca <kevinhuanca75@gmail.com>
2023-06-20 16:51:30 -0300

    BD con datos cargados

--- fravemaxelectrodomesticos.sql
+++ fravemaxelectrodomesticos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-06-2023 a las 01:57:41
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 20-06-2023 a las 21:50:11
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,11 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`idCliente`, `dni`, `apellido`, `nombre`, `domicilio`, `telefono`, `estado`) VALUES
-(1, 3333333, 'AAAAAA', 'BBB', 'PLATINAS 320', '324124124', 1);
+(1, 35900321, 'MESSI', 'LIONEL', 'AYACUCHO 420', '2665236712', 1),
+(2, 12434321, 'LEGRAND', 'MIRTA', '9 DE JULIO 777', '1121215500', 1),
+(3, 40203421, 'GONZALEZ', 'PABLO', 'SAN MARTIN 321', '2664347854', 1),
+(4, 37876123, 'ARJONA', 'RICARDO', 'RIVADAVIA 1289', '2664123321', 1),
+(5, 29088567, 'RINCON', 'GABRIELA', 'LAVALLE 345', '2665789002', 1);
 
 -- --------------------------------------------------------
 
@@ -104,9 +108,16 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`idProducto`, `nombre`, `categoria`, `precio`, `stock`, `estado`) VALUES
-(3, 'MICROONDAS', 'Electrodomestico', 5200, 0, 1),
-(4, 'RINCONERA', 'Muebles', 50000, 0, 1),
-(5, 'JUEGO DE TOALLAS X3', 'HIGIENE', 1400, 0, 1);
+(1, 'DREAN DR280F', 'HELADERA', 182800, 0, 1),
+(2, 'WHIRLPOOL WR80', 'HELADERA', 580900, 0, 1),
+(3, 'BGH QUICK CHEF B120', 'MICROONDAS', 42900, 0, 1),
+(4, 'ATMA MD1723', 'MICROONDAS', 51900, 0, 1),
+(5, 'PEABODY M53', 'COCINA', 86900, 0, 1),
+(6, 'EXPRESSO SPICA SP-1740', 'CAFETERA', 64900, 0, 1),
+(7, 'LILIANA SMARTY AC964', 'CAFETERA', 21000, 0, 1),
+(8, 'PHILCO PHS25HA', 'AIRE AC.', 209000, 0, 1),
+(9, 'ALASKA AS26W', 'AIRE AC.', 334900, 0, 1),
+(10, 'SAMSUNG WW65NO', 'LAVARROPA', 185500, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -121,6 +132,17 @@ CREATE TABLE `proveedor` (
   `telefono` varchar(15) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`idProvedor`, `razonSocial`, `domicilio`, `telefono`, `estado`) VALUES
+(1, 'ELECTRO5', 'MATURIN 2729, CABA', '45844321', 1),
+(2, 'GRUPO FIXON', 'AV. MONTES DE OCA 2044', '1160099888', 1),
+(3, 'TIOMUSA', 'ARANGUREN 1853', '1146323333', 1),
+(4, 'MAYORISTA OMEGA', 'PASTEUR 388', '111554825015', 1),
+(5, 'MULTIPOINT', 'AV. CORDOBA 4860', '3514287623', 1);
 
 -- --------------------------------------------------------
 
@@ -195,7 +217,7 @@ ALTER TABLE `venta`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `compra`
@@ -219,13 +241,13 @@ ALTER TABLE `detalleventa`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedor`
 --
 ALTER TABLE `proveedor`
-  MODIFY `idProvedor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProvedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `venta`
